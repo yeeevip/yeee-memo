4bf45bc7f9d3209aea066eeb248cef8737efaf40
qhl <quhailong1995@163.com>
2022-07-12 15:04:17 +0800

    upd：common-web-拦截器、过滤器

---
+++ common/common-web/src/main/java/vip/yeee/memo/integrate/common/web/config/CommonWebAutoConfig.java
@@ -0,0 +1,13 @@
+package vip.yeee.memo.integrate.common.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * description......
+ *
+ * @author yeeee
+ * @since 2022/7/12 14:21
+ */
+@ComponentScan("vip.yeee.memo.integrate.common.web")
+public class CommonWebAutoConfig {
+}
---
+++ common/common-web/src/main/java/vip/yeee/memo/integrate/common/web/filter/FilterDemo.java
@@ -0,0 +1,26 @@
+package vip.yeee.memo.integrate.common.web.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * 自定义过滤器
+ *
+ * @author yeeee
+ * @since 2022/7/12 14:25
+ */
+@Slf4j
+public class FilterDemo implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("【自定义过滤器】 - 执行前 - uri = {}", ((HttpServletRequest)servletRequest).getRequestURI());
+        // 放行
+        filterChain.doFilter(servletRequest, servletResponse);
+        log.info("【自定义过滤器】 - 执行后 - uri = {}", ((HttpServletRequest)servletRequest).getRequestURI());
+    }
+
+}
