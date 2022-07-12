772dab65cbb17cd5c0c4b48878a33308e0a93d5a
qhl <quhailong1995@163.com>
2022-07-12 19:04:17 +0800

    upd：项目目录结构调整

---
+++ spring-cloud/gateway/springcloud-gateway/src/main/java/vip/yeee/memo/integrate/springcloud/gateway/cloudgateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,17 @@
+package vip.yeee.memo.integrate.springcloud.gateway.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+//@SpringCloudApplication
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
+
+}
