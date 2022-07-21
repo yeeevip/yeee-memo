46130afa4a247be88e6bed77b3129c22daee4d96
qhl <quhailong1995@163.com>
2022-07-21 10:47:05 +0800

    upd：design-pattern 策略模式

---
+++ design-pattern/src/main/java/vip/yeee/memo/integrate/design/DesignPatternApplication.java
@@ -0,0 +1,19 @@
+package vip.yeee.memo.integrate.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description......
+ *
+ * @author yeeee
+ * @since 2022/7/21 10:39
+ */
+@SpringBootApplication
+public class DesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternApplication.class, args);
+    }
+
+}
