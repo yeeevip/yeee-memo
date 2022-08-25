e3050bb811794d1a5ee7301b51b92fe4f7089f60
qhl <quhailong1995@163.com>
2022-08-25 19:13:32 +0800

    upd：使用elasticsearchRestTemplate进行聚合操作

--- middle-ware/elasticsearch/src/test/java/vip/yeee/memo/integrate/elasticsearch/ElasticsearchRepositoryTest.java
+++ middle-ware/elasticsearch/src/test/java/vip/yeee/memo/integrate/elasticsearch/ElasticsearchRepositoryTest.java
@@ -1,6 +1,17 @@
 package vip.yeee.memo.integrate.elasticsearch;
 
-import org.elasticsearch.common.unit.Fuzziness;
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.date.DatePattern;
+import com.google.common.collect.Maps;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.histogram.*;
+import org.elasticsearch.search.aggregations.bucket.range.ParsedDateRange;
+import org.elasticsearch.search.aggregations.bucket.range.ParsedRange;
+import org.elasticsearch.search.aggregations.bucket.range.RangeAggregationBuilder;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
@@ -22,6 +33,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -47,18 +59,27 @@ public class ElasticsearchRepositoryTest {
         log.info("系统初始化会自动扫描实体类中的@Document注解创建索引");
     }
 
+    /**
+     * 判断索引存在
+     */
     @Test
     public void testExistsIndex() {
         boolean flag = elasticsearchRestTemplate.indexOps(TProjectIndex.class).exists();
         log.info("flag = {}", flag);
     }
 
+    /**
+     * 删除索引
+     */
     @Test
     public void testDeleteIndex() {
         boolean flag = elasticsearchRestTemplate.indexOps(TProjectIndex.class).delete();
         log.info("flag = {}", flag);
     }
 
+    /**
+     * 删除索引数据
+     */
     @Test
     public void testDeleteData() {
         // 数据库中的数据
@@ -70,6 +91,9 @@ public class ElasticsearchRepositoryTest {
         projectIndexRepository.deleteAllById(idList);
     }
 
+    /**
+     * 批量同步到ES
+     */
     @Test
     public void testBatchSaveData() {
         // 数据库中的数据
@@ -89,6 +113,9 @@ public class ElasticsearchRepositoryTest {
         log.info("-------------bulk res = {}------------------", res);
     }
 
+    /**
+     * 查找全部
+     */
     @Test
     public void testFindAllData() {
         Page<TProjectIndex> res = (Page<TProjectIndex>) projectIndexRepository.findAll();
@@ -166,23 +193,172 @@ public class ElasticsearchRepositoryTest {
     /**
      * 【聚合查询】
      *
-     * 比较常用的一些度量聚合方式：
-     *
-     * Avg Aggregation：求平均值
-     * Max Aggregation：求最大值
-     * Min Aggregation：求最小值
-     * Percentiles Aggregation：求百分比
-     * Stats Aggregation：同时返回avg、max、min、sum、count等
-     * Sum Aggregation：求和
-     * Top hits Aggregation：求前几
-     * Value Count Aggregation：求总数
+     * 【Terms Aggregation】：根据词条内容分组，词条内容完全匹配的为一组
      *
      */
     @Test
-    public void aggregationSearch() {
+    public void testTermsAggregationSearch() {
 
+        /* --------------------------------------词条桶：Terms Aggregation-------------------------------------- */
+        // eg.根据类型分组，并统计每组的个数及某一属性的最大、最小、平均值...
+        NativeSearchQueryBuilder termsNativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+        // 分桶
+        TermsAggregationBuilder termsAggregationBuilder = AggregationBuilders.terms("type_group").field("categoryId");
+        // 桶内统计
+        termsAggregationBuilder.subAggregation(AggregationBuilders.count("count_val").field("id"));
+        termsAggregationBuilder.subAggregation(AggregationBuilders.avg("avg_val").field("id"));
+        termsAggregationBuilder.subAggregation(AggregationBuilders.min("min_val").field("id"));
+        termsAggregationBuilder.subAggregation(AggregationBuilders.max("max_val").field("id"));
+        termsAggregationBuilder.subAggregation(AggregationBuilders.sum("sum_val").field("id"));
+        // 执行查询
+        termsNativeSearchQueryBuilder.withAggregations(termsAggregationBuilder);
+        SearchHits<TProjectIndex> searchHits = elasticsearchRestTemplate.search(termsNativeSearchQueryBuilder.build(), TProjectIndex.class);
 
+        if (searchHits.getAggregations() == null) {
+            return;
+        }
+        // 处理结果
+        Aggregations aggregations = (Aggregations) searchHits.getAggregations().aggregations();
+        List<Map<String, Object>> termsGroupData = ((Terms)aggregations.get("type_group"))
+                .getBuckets()
+                .stream()
+                .map(bucket -> {
+                    Map<String, Object> map = Maps.newHashMap();
+                    map.put("key", bucket.getKey());
+                    map.put("count", bucket.getDocCount());
+                    if (CollectionUtil.isNotEmpty(bucket.getAggregations())) {
+                        Aggregations subAgg = bucket.getAggregations();
+                        map.put("count_val", ((ParsedValueCount)subAgg.get("count_val")).getValue());
+                        map.put("avg_val", ((ParsedAvg)subAgg.get("avg_val")).getValue());
+                        map.put("min_val", ((ParsedMin)subAgg.get("min_val")).getValue());
+                        map.put("max_val", ((ParsedMax)subAgg.get("max_val")).getValue());
+                        map.put("sum_val", ((ParsedSum)subAgg.get("sum_val")).getValue());
+                    }
+                    return map;
+                })
+                .collect(Collectors.toList());
+        log.info("termsGroupData = {}", termsGroupData);
+    }
 
+    /**
+     * 【聚合查询】
+     *
+     * 【Histogram Aggregation】：根据数值阶梯分组，与日期类似
+     *
+     */
+    @Test
+    public void testHistogramAggregationSearch() {
+
+        /* --------------------------------------阶梯桶：Histogram Aggregation-------------------------------------- */
+        // eg. 按照价格区间[20]分组统计个数
+        NativeSearchQueryBuilder histogramNativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+        //区间分桶
+        HistogramAggregationBuilder histogramAggregationBuilder = AggregationBuilders.histogram("price_histogram").field("id").interval(20);
+        histogramNativeSearchQueryBuilder.withAggregations(histogramAggregationBuilder);
+        // 查询
+        SearchHits<TProjectIndex> searchHits = elasticsearchRestTemplate.search(histogramNativeSearchQueryBuilder.build(), TProjectIndex.class);
+        // 结果处理
+        if (searchHits.getAggregations() ==null) {
+            return;
+        }
+        Aggregations aggregations = (Aggregations)searchHits.getAggregations().aggregations();
+        List<Map<String, Object>> histogramDataGroup = ((ParsedHistogram) aggregations.get("price_histogram"))
+                .getBuckets()
+                .stream()
+                .map(bucket -> {
+                    Map<String, Object> map = Maps.newHashMap();
+                    map.put("key", bucket.getKey());
+                    map.put("count", bucket.getDocCount());
+                    return map;
+                })
+                .collect(Collectors.toList());
+        log.info("termsGroupData = {}", histogramDataGroup);
+    }
+
+    /**
+     * 【聚合查询】
+     *
+     * 【Date Histogram Aggregation】：根据日期阶梯分组，例如给定阶梯为周，会自动每周分为一组
+     *
+     */
+    @Test
+    public void testDateHistogramAggregationSearch() {
+
+        /* --------------------------------------Date Histogram Aggregation-------------------------------------- */
+        NativeSearchQueryBuilder dateHistogramNativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+        // 按年月份进行分组
+        DateHistogramAggregationBuilder dateHistogramAggregationBuilder = AggregationBuilders.dateHistogram("createTime_histogram")
+                .field("createTime")
+                .calendarInterval(DateHistogramInterval.MONTH)
+                .minDocCount(1);
+        dateHistogramNativeSearchQueryBuilder.withAggregations(dateHistogramAggregationBuilder);
+        SearchHits<TProjectIndex> searchHits = elasticsearchRestTemplate.search(dateHistogramNativeSearchQueryBuilder.build(), TProjectIndex.class);
+        if (searchHits.getAggregations() == null) {
+            return;
+        }
+        Aggregations aggregations = (Aggregations)searchHits.getAggregations().aggregations();
+        List<Map<String, Object>> dateHistogramGroupData = ((ParsedDateHistogram) aggregations.get("createTime_histogram"))
+                .getBuckets()
+                .stream()
+                .map(bucket -> {
+                    Map<String, Object> map = Maps.newHashMap();
+                    map.put("key", bucket.getKey());
+                    map.put("count", bucket.getDocCount());
+                    return map;
+                })
+                .collect(Collectors.toList());
+        log.info("dateHistogramGroupData = {}", dateHistogramGroupData);
+    }
+
+    /**
+     * 【聚合查询】
+     *
+     * 【Range Aggregation】：数值和日期的[范围]分组，指定开始和结束，然后按段分组
+     *
+     */
+    @Test
+    public void testRangeAggregationSearch() {
+
+        /* --------------------------------------Range Aggregation-------------------------------------- */
+        // eg. 指定数值范围200-300、时间范围
+        NativeSearchQueryBuilder rangeNativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+        //范围分桶
+        RangeAggregationBuilder rangeAggregationBuilder = AggregationBuilders.range("price_range").field("id").addRange(200, 300);
+        rangeAggregationBuilder.subAggregation(AggregationBuilders.dateRange("date_range").field("createTime")
+                        .format(DatePattern.NORM_DATETIME_PATTERN)
+                .addRange("2010-01-01 00:00:00", "2020-01-01 00:00:00").addRange("2020-01-01 00:00:00", "2022-01-01 00:00:00"));
+        rangeNativeSearchQueryBuilder.withAggregations(rangeAggregationBuilder);
+        // 查询
+        SearchHits<TProjectIndex> searchHits = elasticsearchRestTemplate.search(rangeNativeSearchQueryBuilder.build(), TProjectIndex.class);
+        // 结果处理
+        if (searchHits.getAggregations() ==null) {
+            return;
+        }
+        Aggregations aggregations = (Aggregations)searchHits.getAggregations().aggregations();
+        List<Map<String, Object>> rangeDataGroup = ((ParsedRange) aggregations.get("price_range"))
+                .getBuckets()
+                .stream()
+                .map(bucket -> {
+                    Map<String, Object> map = Maps.newHashMap();
+                    map.put("key", bucket.getKey());
+                    map.put("count", bucket.getDocCount());
+                    if (bucket.getAggregations() != null) {
+                        List<Map<String, Object>> subDateRange = ((ParsedDateRange) bucket.getAggregations().get("date_range"))
+                                .getBuckets()
+                                .stream()
+                                .map(bucket1 -> {
+                                    Map<String, Object> map1 = Maps.newHashMap();
+                                    map1.put("key", bucket1.getKey());
+                                    map1.put("count", bucket1.getDocCount());
+                                    return map1;
+                                })
+                                .collect(Collectors.toList());
+                        map.put("subDateRange", subDateRange);
+                    }
+                    return map;
+                })
+                .collect(Collectors.toList());
+        log.info("rangeDataGroup = {}", rangeDataGroup);
     }
 
 }
