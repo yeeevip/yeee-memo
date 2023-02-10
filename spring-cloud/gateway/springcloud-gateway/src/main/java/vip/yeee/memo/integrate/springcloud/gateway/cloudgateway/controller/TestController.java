3ca1c08bda96b0c25d0ab0f828269fafa2636b08
qhl <quhailong1995@163.com>
2023-02-10 09:53:51 +0800

    add：Nacos @ConfigurationProperties可以直接刷新，@Value需要加@RefreshScope

---
+++ spring-cloud/gateway/springcloud-gateway/src/main/java/vip/yeee/memo/integrate/springcloud/gateway/cloudgateway/controller/TestController.java
@@ -0,0 +1,27 @@
+package vip.yeee.memo.integrate.springcloud.gateway.cloudgateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vip.yeee.memo.integrate.base.model.rest.CommonResult;
+import vip.yeee.memo.integrate.springcloud.gateway.cloudgateway.component.TestBiz;
+
+import javax.annotation.Resource;
+
+/**
+ * description......
+ *
+ * @author yeeee
+ * @since 2023/2/10 9:31
+ */
+@RestController
+public class TestController {
+
+    @Resource
+    private TestBiz testBiz;
+
+    @GetMapping("/gw/test/config-change")
+    public CommonResult<Object> testConfigPropertyChange() {
+        return CommonResult.success(testBiz.testConfigPropertyChange());
+    }
+
+}
