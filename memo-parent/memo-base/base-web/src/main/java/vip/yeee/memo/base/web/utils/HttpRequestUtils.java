b560a38c63cb20a0837d85c19563077cce15fa96
qhl <quhailong1995@163.com>
2023-07-18 10:16:20 +0800

    add：HttpRequestUtils

---
+++ memo-parent/memo-base/base-web/src/main/java/vip/yeee/memo/base/web/utils/HttpRequestUtils.java
@@ -0,0 +1,127 @@
+package vip.yeee.memo.base.web.utils;
+
+import cn.hutool.core.util.StrUtil;
+import org.springframework.http.HttpHeaders;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ * <p>
+ * http内容 工具类
+ * </p>
+ *
+ * @author yeah丶一页 (quhailong1995@gmail.com)
+ */
+public class HttpRequestUtils {
+    
+    private static final String LOCALHOST = "127.0.0.1";
+    
+	public static HttpServletRequest getHttpServletRequest() {
+		return SpringContextUtils.getHttpServletRequest();
+	}
+
+	public static Map<String, String> getParameterMap(HttpServletRequest request) {
+		Enumeration<String> parameters = request.getParameterNames();
+
+		Map<String, String> params = new HashMap<>();
+		while (parameters.hasMoreElements()) {
+			String parameter = parameters.nextElement();
+			String value = request.getParameter(parameter);
+			if (StrUtil.isNotBlank(value)) {
+				params.put(parameter, value);
+			}
+		}
+		return params;
+	}
+
+	public static String getDomain() {
+		HttpServletRequest request = getHttpServletRequest();
+		StringBuffer url = request.getRequestURL();
+		return url.delete(url.length() - request.getRequestURI().length(), url.length()).toString();
+	}
+
+	public static String getOrigin() {
+		HttpServletRequest request = getHttpServletRequest();
+		return request.getHeader(HttpHeaders.ORIGIN);
+	}
+
+	public static String getLanguage() {
+		//默认语言
+		String defaultLanguage = "zh-CN";
+		//request
+		HttpServletRequest request = getHttpServletRequest();
+
+        //请求语言
+		defaultLanguage = request.getHeader(HttpHeaders.ACCEPT_LANGUAGE);
+
+		return defaultLanguage;
+	}
+	
+	public static String getUserAgent(HttpServletRequest request) {
+	    return request.getHeader("User-Agent");
+	}
+
+	public static String getReferer(HttpServletRequest request) {
+	    return request.getHeader("Referer");
+	}
+	
+	/**
+     * 获取IP地址 使用Nginx等反向代理软件， 则不能通过request.getRemoteAddr()获取IP地址
+     * 如果使用了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP地址，X-Forwarded-
+     * For中第一个非unknown的有效IP字符串，则为真实IP地址
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (StrUtil.isBlank(ip) || "unknown".equalsIgnoreCase(ip)|| "127.0.0.1".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (StrUtil.isBlank(ip) || "unknown".equalsIgnoreCase(ip)|| "127.0.0.1".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (StrUtil.isBlank(ip) || "unknown".equalsIgnoreCase(ip)|| "127.0.0.1".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+        }
+        if (StrUtil.isBlank(ip) || "unknown".equalsIgnoreCase(ip)|| "127.0.0.1".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if (StrUtil.isBlank(ip) ||"127.0.0.1".equals(ip)|| ip.contains(":")) {
+            try {
+                ip = InetAddress.getLocalHost().getHostAddress();
+            } catch (UnknownHostException e) {
+                ip = null;
+            }
+        }
+        // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ip != null && ip.length() > 15) {
+            if (ip.indexOf(",") > 0) {
+                ip = ip.substring(0, ip.indexOf(","));
+            }
+        }
+        return ip;
+    }
+    
+    /**
+     * 获取请求basePath
+     */
+    public static String getBasePath(HttpServletRequest request) {
+        StringBuilder basePath = new StringBuilder();
+        String scheme = request.getScheme();
+        String domain = request.getServerName();
+        int port = request.getServerPort();
+        basePath.append(scheme);
+        basePath.append("://");
+        basePath.append(domain);
+        if ("http".equalsIgnoreCase(scheme) && 80 != port) {
+            basePath.append(":").append(port);
+        } else if ("https".equalsIgnoreCase(scheme) && port != 443) {
+            basePath.append(":").append(port);
+        }
+        return basePath.toString();
+    }
+}
