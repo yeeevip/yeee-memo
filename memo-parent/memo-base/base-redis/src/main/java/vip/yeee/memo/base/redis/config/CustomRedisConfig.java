cb0674267e6c0069630d89f2f596b6d8d00a76cb
qhl <quhailong1995@163.com>
2023-09-27 10:14:27 +0800

    upd：springboot 自定义redis数据源

--- memo-parent/memo-base/base-redis/src/main/java/vip/yeee/memo/base/redis/config/CustomRedisConfig.java
+++ memo-parent/memo-base/base-redis/src/main/java/vip/yeee/memo/base/redis/config/CustomRedisConfig.java
@@ -17,13 +17,17 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import vip.yeee.memo.base.redis.properties.CustomRedisProperty;
 
 import javax.annotation.Resource;
+import java.time.Duration;
 
 
 /**
@@ -53,30 +57,10 @@ public class CustomRedisConfig implements InitializingBean, ApplicationContextAw
 
             String beanName = customRedisProperties.getBeanName();
 
-            RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration ();
-            redisStandaloneConfiguration.setHostName(customRedisProperties.getHost());
-            redisStandaloneConfiguration.setPort(customRedisProperties.getPort());
-            redisStandaloneConfiguration.setDatabase(customRedisProperties.getDatabase());
-            redisStandaloneConfiguration.setPassword(customRedisProperties.getPassword());
-            LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
-            lettuceConnectionFactory.afterPropertiesSet(); // 初始化连接工厂
-
-            //设置序列化
-            ObjectMapper om = new ObjectMapper();
-            om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-            om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-            om.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-            Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
-            jackson2JsonRedisSerializer.setObjectMapper(om);
-
-            RedisTemplate<String, Object> template = new RedisTemplate<>();
-            template.setConnectionFactory(lettuceConnectionFactory);
-            template.setKeySerializer(new StringRedisSerializer());
-            template.setValueSerializer(jackson2JsonRedisSerializer);
 
             GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
             beanDefinition.setBeanClass(RedisTemplate.class);
-            beanDefinition.setInstanceSupplier(() -> template);
+            beanDefinition.setInstanceSupplier(() -> getRedisTemplate(customRedisProperties));
 
             ((GenericApplicationContext)applicationContext).registerBeanDefinition(beanName, beanDefinition);
 
@@ -88,4 +72,43 @@ public class CustomRedisConfig implements InitializingBean, ApplicationContextAw
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
     }
+
+    public RedisTemplate<String, Object> getRedisTemplate(CustomRedisProperty.CustomRedisProperties customRedisProperties) {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration ();
+        redisStandaloneConfiguration.setHostName(customRedisProperties.getHost());
+        redisStandaloneConfiguration.setPort(customRedisProperties.getPort());
+        redisStandaloneConfiguration.setDatabase(customRedisProperties.getDatabase());
+        redisStandaloneConfiguration.setPassword(customRedisProperties.getPassword());
+
+//        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+//        jedisClientConfiguration.connectTimeout(Duration.ofMillis(0));
+//        JedisConnectionFactory connectionFactory = new JedisConnectionFactory(redisStandaloneConfiguration,
+//                jedisClientConfiguration.build());
+
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        connectionFactory.afterPropertiesSet(); // 初始化连接工厂
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        RedisSerializer<?> stringSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+
+        //设置序列化
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        om.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        template.setDefaultSerializer(jackson2JsonRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
 }
