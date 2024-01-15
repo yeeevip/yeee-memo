d04d3b66590e418f3f8e219e3dc141d2278f7315
qhl <quhailong1995@163.com>
2024-01-15 10:20:36 +0800

    upd：simple-tools-my

---
+++ simple-tools/simple-tools-my/src/main/java/vip/yeee/memo/demo/simpletool/my/temp/StringDataConvert.java
@@ -0,0 +1,30 @@
+package vip.yeee.memo.demo.simpletool.my.temp;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * description......
+ *
+ * @author yeeee
+ * @since 2024/1/15 10:19
+ */
+public class StringDataConvert {
+
+    public static void main(String[] args) {
+
+        String aa = "";
+
+
+
+        System.out.println(Stream.of(aa.replaceAll("【", "").replaceAll("】", "").split("[\n,]"))
+                .map(s -> "'" + s + "'")
+//                .map(s -> "'\"" + s + "\"'")
+//                .map(s -> "DATA_CONVERTER:LAST:TRANSFER_CONTENT-RELATE:bfeb4822c0824bc9ac48a588dda9ee12:" + s)
+                .distinct()
+                .collect(Collectors.joining(",")));
+//                .collect(Collectors.joining(" ")));
+
+    }
+
+}
