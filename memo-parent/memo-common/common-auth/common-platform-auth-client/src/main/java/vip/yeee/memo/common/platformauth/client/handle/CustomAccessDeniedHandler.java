87d96c27dd1b55dd3b0291f7e0c4ad9e19a8a67a
qhl <quhailong1995@163.com>
2025-02-09 14:17:30 +0800

    upd：

--- memo-parent/memo-common/common-auth/common-platform-auth-client/src/main/java/vip/yeee/memo/common/platformauth/client/handle/CustomAccessDeniedHandler.java
+++ memo-parent/memo-common/common-auth/common-platform-auth-client/src/main/java/vip/yeee/memo/common/platformauth/client/handle/CustomAccessDeniedHandler.java
@@ -1,5 +1,6 @@
 package vip.yeee.memo.common.platformauth.client.handle;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import vip.yeee.memo.base.model.rest.CommonResult;
@@ -13,10 +14,12 @@ import java.io.IOException;
 /**
  * 处理器
  */
+@Slf4j
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException, ServletException {
+        log.warn("[YEEE认证] - 未登录或token失效，url = {}", request.getRequestURI());
         HttpResponseUtils.write(response, CommonResult.forbidden(""));
     }
 
--- memo-parent/memo-common/common-auth/common-platform-auth-client/src/main/java/vip/yeee/memo/common/platformauth/client/handle/CustomAuthenticationEntryPoint.java
+++ memo-parent/memo-common/common-auth/common-platform-auth-client/src/main/java/vip/yeee/memo/common/platformauth/client/handle/CustomAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package vip.yeee.memo.common.platformauth.client.handle;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import vip.yeee.memo.base.model.rest.CommonResult;
@@ -13,10 +14,12 @@ import java.io.IOException;
 /**
  * 处理器
  */
+@Slf4j
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+        log.warn("[YEEE认证] - 未登录或token失效，url = {}", request.getRequestURI());
         HttpResponseUtils.write(response, CommonResult.unauthorized(""));
     }
 
